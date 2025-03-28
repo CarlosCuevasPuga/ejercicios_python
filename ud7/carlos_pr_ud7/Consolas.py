d852d93d329853a7d7578e9408f32c954bdbdc0c
Carlos Cuevas Puga <ccuepug185@g.educaand.es>
2025-03-28 21:21:26 +0100

    comienzo de la practica de la ud7

---
+++ ud7/carlos_pr_ud7/Consolas.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+class Consolas:
+    def __init__(self, nombre: str, desarrolladores: str, direccion_mac: str, fecha_salida: datetime, color: str):
+        self.nombre = nombre
+        self.desarrolladores = desarrolladores
+        self.direccion_mac = direccion_mac
+        if fecha_salida < datetime.now():
+            self.fecha_salida = fecha_salida
+        else:
+            self.fecha_salida = datetime.now()
+        self.color = color
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Consolas):
+            return self.direccion_mac == other.direccion_mac
+        return False
+
+    def __lt__(self, other) -> bool:
+        return self.fecha_salida < other.fecha_salida
+
+    def __le__(self, other) -> bool:
+        return self.fecha_salida <= other.fecha_salida
+
+    def __gt__(self, other) -> bool:
+        return self.fecha_salida > other.fecha_salida
+
+    def __ge__(self, other) -> bool:
+        return self.fecha_salida >= other.fecha_salida
