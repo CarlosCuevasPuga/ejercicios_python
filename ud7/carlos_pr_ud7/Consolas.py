f7c77b972f81bdf77473aa76a3ee5ef1e0bf73cd
Carlos Cuevas Puga <ccuepug185@g.educaand.es>
2025-04-04 20:40:09 +0200

    practica UD7 (en desarrollo)

--- ud7/carlos_pr_ud7/Consolas.py
+++ ud7/carlos_pr_ud7/Consolas.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 class Consolas:
-    def __init__(self, nombre: str, desarrolladores: str, direccion_mac: str, fecha_salida: datetime, color: str):
+    def __init__(self, nombre: str, desarrolladores: str, direccion_mac: str, fecha_salida: datetime, colores: list):
         self.nombre = nombre
         self.desarrolladores = desarrolladores
         self.direccion_mac = direccion_mac
@@ -9,7 +9,7 @@ class Consolas:
             self.fecha_salida = fecha_salida
         else:
             self.fecha_salida = datetime.now()
-        self.color = color
+        self.colores = colores
 
     def __eq__(self, other) -> bool:
         if isinstance(other, Consolas):
