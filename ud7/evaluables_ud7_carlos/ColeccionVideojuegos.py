10215cc48d1cf2f17c6f18204ffa1518eb9cb8f8
Carlos Cuevas Puga <ccuepug185@g.educaand.es>
2025-04-09 16:16:37 +0200

    Acabada la revision del archivo ColeccionVideojuegos (juegos_repetidos)

--- ud7/evaluables_ud7_carlos/ColeccionVideojuegos.py
+++ ud7/evaluables_ud7_carlos/ColeccionVideojuegos.py
@@ -14,22 +14,41 @@ class ColeccionVideojuegos:
         return coleccion_str
 
     def juegos_repetidos(self) -> list:
-        no_repetidos = []
+        
+        def comparar(j1: EjemplarVideojuego, j2: EjemplarVideojuego) -> bool:
+            return j1.nombre == j2.nombre and j1.fecha_salida == j2.fecha_salida
+        
+        def contiene(juego: EjemplarVideojuego, juegos: list) -> bool:
+            lo_contiene = False
+            for j in juegos:
+                if comparar(juego, j):
+                    lo_contiene = True
+                    break
+            return lo_contiene
+        
         repetidos = []
-        for juego in self.videojuegos:
-            if [juego.nombre, juego.fecha_salida] not in no_repetidos:
-                no_repetidos.append([juego.nombre, juego.fecha_salida])
+        no_repetidos = []
+
+        for j in self.coleccion:
+            if not contiene(j, no_repetidos):
+                no_repetidos.append(j)
             else:
-                if juego not in repetidos:
-                    repetidos.append(juego)
-                    
+                if not contiene(j, repetidos):
+                    repetidos.append(j)
+       
         return repetidos
+
 
     def mostrar_juegos_repetidos(self):
         repetidos = self.juegos_repetidos()
+
         if repetidos:
+            print("Los juegos repetidos son: ")
+
             for juego in repetidos:
-                print(f"Juego repetido: {juego}")
+                print(juego.nombre)
+        else:
+            print("No hay juegos repetidos")
 
     def eliminar_juego(self, id: int):
         self.videojuegos = [juego for juego in self.videojuegos if juego.id != id]
